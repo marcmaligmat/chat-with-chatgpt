85e1891e8d05666ca0b7cb16d7135bbb19b458bb
Marc <marcmaligmat@gmail.com>
2023-08-14 17:52:58 +0800

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-export function ModeToggle() {
+export default function Home() {
   const { setTheme } = useTheme();
 
   return (
